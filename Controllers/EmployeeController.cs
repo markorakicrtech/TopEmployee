42d8b909455796f00a16c0bf42baa4568b6ac04d
Marko Rakic <mrakic86@gmail.com>
2020-01-16 23:43:32 +0100

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TopEmployee.Models;
+
+namespace TopEmployee.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ViewResult Register()
+        {
+            Employee e = new Employee();
+            return View(e);
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TopEmployee.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
